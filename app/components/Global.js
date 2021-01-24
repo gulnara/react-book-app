1e94ca634d94213f8a1baae6b2f647a63070e0a4
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2021-01-24 18:05:57 -0500

    added onclick event

--- app/components/Global.js
+++ app/components/Global.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
+import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
 
 class Global extends Component {
+	search() {
+		console.log('search');
+	}
+
 	render() {
 		return (
-			<div>Global app Component.</div>
+			<div>
+				<h2>Book Explorer!</h2>
+				<FormGroup>
+					<InputGroup className="mb-3">
+						<FormControl type="text" placeholder="Search for a book."/>
+				    <InputGroup.Append onClick={() => this.search()}>
+				      <InputGroup.Text id="basic-addon2">search</InputGroup.Text>
+				    </InputGroup.Append>
+					</InputGroup>
+				</FormGroup>
+			</div>
 		)
 	}
 }
