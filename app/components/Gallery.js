39ae0f1b00bdb1f68e7110f20006957962232175
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2021-01-24 18:51:27 -0500

    added links

--- app/components/Gallery.js
+++ app/components/Gallery.js
@@ -8,7 +8,12 @@ class Gallery extends Component {
 					this.props.items.map((item, index) => {
 						let { title, imageLinks, infoLink } = item.volumeInfo;
 						return (
-							<div key={index} className="book">
+							<a 
+								key={index} 
+								className="book"
+								href={infoLink}
+								target="_blank"
+							>
 							<img 
 								src={imageLinks !== undefined ? imageLinks.thumbnail : ''} 
 								alt="book image"
@@ -17,7 +22,7 @@ class Gallery extends Component {
 								<div className="book-text">
 									{title}
 								</div>
-							</div>
+							</a>
 						)
 					})
 				}
