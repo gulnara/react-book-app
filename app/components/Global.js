198c85ab6ee104a2b28a32338b2a9eb29484d676
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2021-01-24 18:11:40 -0500

    added constructor and state

--- app/components/Global.js
+++ app/components/Global.js
@@ -2,8 +2,16 @@ import React, { Component } from 'react';
 import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
 
 class Global extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			query: ''
+		}
+	}
+
+
 	search() {
-		console.log('search');
+		console.log('search', this.state.query);
 	}
 
 	render() {
@@ -12,7 +20,11 @@ class Global extends Component {
 				<h2>Book Explorer!</h2>
 				<FormGroup>
 					<InputGroup className="mb-3">
-						<FormControl type="text" placeholder="Search for a book."/>
+						<FormControl 
+						type="text" 
+						placeholder="Search for a book."
+						onChange={event => this.setState({query: event.target.value})}
+						/>
 				    <InputGroup.Append onClick={() => this.search()}>
 				      <InputGroup.Text id="basic-addon2">search</InputGroup.Text>
 				    </InputGroup.Append>
