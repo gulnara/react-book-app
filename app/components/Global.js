97b868df2f46069a68fc1e481ced7a079c825445
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2021-01-24 18:27:11 -0500

    fetching books from google

--- app/components/Global.js
+++ app/components/Global.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
+import { Form, FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
 
 class Global extends Component {
 	constructor(props) {
@@ -11,30 +11,35 @@ class Global extends Component {
 
 
 	search() {
-		console.log('search', this.state.query);
+		const BASE_URL = 'https://www.googleapis.com/books/v1/volumes?q=';
+		fetch(`${BASE_URL}${this.state.query}`, { method: 'GET'})
+			.then(response => response.json())
+			.then(json => console.log(json));
 	}
 
 	render() {
 		return (
-			<div>
+			<div className="Global">
 				<h2>Book Explorer!</h2>
-				<FormGroup>
-					<InputGroup className="mb-3">
-						<FormControl 
-						type="text" 
-						placeholder="Search for a book."
-						onChange={event => this.setState({query: event.target.value})}
-						onKeyPress={event => {
-							if (event.key === 'Enter') {
-								this.search();
-							}
-						}}
-						/>
-				    <InputGroup.Append onClick={() => this.search()}>
-				      <InputGroup.Text id="basic-addon2">search</InputGroup.Text>
-				    </InputGroup.Append>
-					</InputGroup>
-				</FormGroup>
+				<Form>
+					<Form.Group>
+						<InputGroup className="mb-3">
+							<Form.Control 
+							type="text" 
+							placeholder="Search for a book."
+							onChange={event => this.setState({query: event.target.value})}
+							onKeyPress={event => {
+								if (event.key === 'Enter') {
+									this.search();
+								}
+							}}
+							/>
+					    <InputGroup.Append onClick={() => this.search()}>
+					      <InputGroup.Text id="basic-addon2">search</InputGroup.Text>
+					    </InputGroup.Append>
+						</InputGroup>
+					</Form.Group>
+				</Form>
 			</div>
 		)
 	}
